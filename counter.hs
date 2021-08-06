a81fb15ffae4f7a36f59846fed2ddbe400c3597e
Vladimir Perepelkin <79280805+dogfew@users.noreply.github.com>
2021-08-06 21:24:22 +0300

    Add files via upload

---
+++ counter.hs
@@ -0,0 +1,7 @@
+counter element x = subcounter element 0 x 
+    where subcounter element number x = case x of 
+            (x:xs) -> if x == element then subcounter element (number + 1) xs
+                                      else subcounter element number       xs
+            [x]    -> if x == element then number + 1 
+                                      else number    
+            []     -> number
